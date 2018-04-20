15dea3ac0e037ecd255ed12e9db0def2a0a8f7df
Gemma Black <gblackuk@googlemail.com>
2018-04-20 22:13:08 +0100

    Github logo added

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -31,6 +31,11 @@ export class App extends React.Component {
 
     render() {
         return <article className="h-100 overflow-y-auto montserrat">
+
+            <a href="https://github.com/gemmadlou/gemmadlou.github.io">
+                <img className="absolute right-2 top-1 z-3" 
+                    src="assets/github.png" width="100" />
+            </a>
             <div className="relative vh-100 dt w-100 bg-dark-gray white cover" 
                 style={{background: `url(https://images.unsplash.com/photo-1514792368985-f80e9d482a02?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b067b70aaa4390e9c1ad2133f406a377&auto=format&fit=crop&w=1050&q=80) no-repeat center`}}>
                 <div className="absolute top-0 w-100 left-0 vh-100 bg-black-50 z-0"></div>
