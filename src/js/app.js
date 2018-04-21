02074fb9d78409e9cd47c06736000fafc57ac697
Gemma Black <gblackuk@googlemail.com>
2018-04-21 14:09:57 +0100

    Service worker added

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import domready from 'domready';
-import { App } from './components/app.jsx';
+import {
+    App
+} from './components/app.jsx';
 
 domready(() => {
-    ReactDOM.render(
-        <App />,
+    ReactDOM.render( <
+        App / > ,
         document.getElementById('app')
-    );    
+    );
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
